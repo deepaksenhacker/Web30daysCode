ec73ac414ae7726387389dda1e036a8faacc2aa7
deepaksenhacker <care.deepaksen@gmail.com>
2024-11-21 20:52:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+
+var bg = document.getElementById('img1')
+
+var img1= document.getElementById('one');
+img1.addEventListener('mouseover',()=>{
+    bg.src =img1.src;
+})
+img1.addEventListener('mouseleave',()=>{
+    bg.src ='./images/1.png';
+})
+
+// two
+
+
+
+
+
+var img2= document.getElementById('two');
+img2.addEventListener('mouseover',()=>{
+    bg.src =img2.src;
+})
+img2.addEventListener('mouseleave',()=>{
+    bg.src ='./images/1.png';
+})
+
+
+
+var img3= document.getElementById('three');
+img3.addEventListener('mouseover',()=>{
+    bg.src =img3.src;
+})
+img3.addEventListener('mouseleave',()=>{
+    bg.src ='./images/1.png';
+})
+
+var img4= document.getElementById('four');
+img4.addEventListener('mouseover',()=>{
+    bg.src =img4.src;
+})
+img4.addEventListener('mouseleave',()=>{
+    bg.src ='./images/1.png';
+})
+
+var img5= document.getElementById('five');
+img5.addEventListener('mouseover',()=>{
+    bg.src =img5.src;
+})
+img5.addEventListener('mouseleave',()=>{
+    bg.src ='./images/1.png';
+})
